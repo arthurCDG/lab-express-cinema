dc8a79381e791ca0f4fc906ffd298e313b96bee4
Arthur Chastenet <arthur.chastenet@gmail.com>
2022-01-06 19:32:56 +0100

    Finished the lab

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,26 @@
-const express = require('express');
+const express = require("express");
+const res = require("express/lib/response");
 const router = express.Router();
+const Movie = require("../models/Movie.model");
 
 /* GET home page */
-router.get('/', (req, res, next) => res.render('index'));
+router.get("/", (req, res, next) => res.render("index"));
+
+router.get("/movies", (req, res, next) => {
+  Movie.find()
+    .then((allMoviesFetched) => {
+      res.render("movies.hbs", { allMoviesFetched });
+    })
+    .catch((error) => console.error(error));
+});
+
+router.get("/movies/:id", (req, res, next) => {
+  const id = req.params.id;
+  Movie.findById(id)
+    .then((singleMovieFetched) => {
+      res.render("single-movie.hbs", { singleMovieFetched });
+    })
+    .catch((error) => console.error(error));
+});
 
 module.exports = router;
